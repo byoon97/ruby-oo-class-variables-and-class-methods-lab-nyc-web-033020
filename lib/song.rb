6d1c08b8fa66eb5c73b473c2999d2fa7ae38fafc
Brandon Yoon <byoon5397@gmail.com>
2020-04-02 06:14:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,15 @@ class Song
   @@artist = []
   @@genres = []
   @@count = 0
-  @@artist_count = {}
-  @@genre_count = {}
 
   attr_accessor = :name, :genre, :artist
 
   def initialize(name, genre, artist)
     @@artist << @artist
     @@genres << @genre
+    @@count += 1
+    @name = name
+    @genre = genre
+    @artist = genre
   end
 end
