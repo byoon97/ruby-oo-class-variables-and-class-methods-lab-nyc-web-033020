30bec32780cce90571b7c8c264fcd80f4c8a0e6e
Brandon Yoon <byoon5397@gmail.com>
2020-04-02 06:08:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,4 +4,11 @@ class Song
   @@count = 0
   @@artist_count = {}
   @@genre_count = {}
+
+  attr_accessor = :name, :genre, :artist
+
+  def initialize(name, genre, artist)
+    @@artist << @artist
+    @@genres << @genre
+  end
 end
